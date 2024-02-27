12c9b51a7470fdeb09ed20043cfe9dbe3c2b1711
Alberto Ferreira <aferreira6064@gmail.com>
2024-02-27 10:17:02 -0300

    control de nullpointer en biometrico

--- DatBiom_AJ/src/datbiom_aj/CapturaHuella.java
+++ DatBiom_AJ/src/datbiom_aj/CapturaHuella.java
@@ -49,11 +49,7 @@ public class CapturaHuella extends javax.swing.JFrame {
         btnGuardar1 = new javax.swing.JButton();
         panHuellas = new javax.swing.JPanel();
         lblImagenHuella = new javax.swing.JLabel();
-        panBtns1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
         btnGuardar = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        txtArea1 = new javax.swing.JTextArea();
 
         panBtns.setBackground(new java.awt.Color(255, 255, 255));
         panBtns.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(41, 43, 45)), "Acciones", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
@@ -104,11 +100,6 @@ public class CapturaHuella extends javax.swing.JFrame {
         panHuellas.setLayout(new java.awt.BorderLayout());
         panHuellas.add(lblImagenHuella, java.awt.BorderLayout.CENTER);
 
-        panBtns1.setBackground(new java.awt.Color(255, 255, 255));
-        panBtns1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(41, 43, 45)), "Acciones", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
-        panBtns1.setPreferredSize(new java.awt.Dimension(400, 250));
-        panBtns1.setLayout(new java.awt.BorderLayout());
-
         btnGuardar.setText("Guardar Nueva Huella");
         btnGuardar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -116,64 +107,35 @@ public class CapturaHuella extends javax.swing.JFrame {
             }
         });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(285, Short.MAX_VALUE)
-                .addComponent(btnGuardar)
-                .addGap(14, 14, 14))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-
-        panBtns1.add(jPanel2, java.awt.BorderLayout.PAGE_END);
-
-        txtArea1.setColumns(20);
-        txtArea1.setFont(new java.awt.Font("Lucida Sans", 1, 10)); // NOI18N
-        txtArea1.setLineWrap(true);
-        txtArea1.setRows(10);
-        txtArea1.setMinimumSize(new java.awt.Dimension(100, 20));
-        txtArea1.setPreferredSize(new java.awt.Dimension(200, 180));
-        jScrollPane2.setViewportView(txtArea1);
-
-        panBtns1.add(jScrollPane2, java.awt.BorderLayout.CENTER);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(panHuellas, javax.swing.GroupLayout.PREFERRED_SIZE, 443, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(panHuellas, javax.swing.GroupLayout.PREFERRED_SIZE, 443, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(14, 14, 14))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panBtns1, javax.swing.GroupLayout.PREFERRED_SIZE, 443, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(panHuellas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panBtns1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(9, Short.MAX_VALUE))
+                .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnGuardar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardar1ActionPerformed
-
+        guardarHuella();
+        Reclutador.clear();
+        lblImagenHuella.setIcon(null);
+        start();
     }//GEN-LAST:event_btnGuardar1ActionPerformed
-
 
     private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
         guardarHuella();
@@ -181,6 +143,9 @@ public class CapturaHuella extends javax.swing.JFrame {
         lblImagenHuella.setIcon(null);
         start();
     }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private DPFPTemplate template;
+    public static String TEMPLATE_PROPERTY = "template";
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -195,10 +160,6 @@ public class CapturaHuella extends javax.swing.JFrame {
 //Varible que permite iniciar el dispositivo de lector de huella conectado con sus distintos metodos.
     private DPFPCapture Lector = DPFPGlobal.getCaptureFactory().createCapture();
 
-    private DPFPTemplate template;
-
-    public static String TEMPLATE_PROPERTY = "template";
-
     public void EnviarTexto(String string) {
         txtArea.append(string + "\n");
     }
@@ -209,20 +170,25 @@ public class CapturaHuella extends javax.swing.JFrame {
     }
 
     public void guardarHuella() {
-        //Obtiene los datos del template de la huella actual
-        ByteArrayInputStream datosHuella = new ByteArrayInputStream(template.serialize());
-        Integer tamañoHuella = template.serialize().length;
-        String datosAGuardar = datosHuella.toString();
-        String rutaArchivo = "datos_guardados.txt";
+        if (template != null) {
+            //Obtiene los datos del template de la huella actual
+            ByteArrayInputStream datosHuella = new ByteArrayInputStream(template.serialize());
+            Integer tamañoHuella = template.serialize().length;
 
-        try {
-            FileWriter fileWriter = new FileWriter(rutaArchivo);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(datosAGuardar);
-            bufferedWriter.close();
-            System.out.println("Datos guardados correctamente en " + rutaArchivo);
-        } catch (IOException e) {
-            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+            String datosAGuardar = datosHuella.toString() + "\n" + tamañoHuella.toString();
+            String rutaArchivo = "datos_guardados.txt";
+
+            try {
+                FileWriter fileWriter = new FileWriter(rutaArchivo);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(datosAGuardar);
+                bufferedWriter.close();
+                System.out.println("Datos guardados correctamente en " + rutaArchivo);
+            } catch (IOException e) {
+                System.out.println("Error al escribir en el archivo: " + e.getMessage());
+            }
+        } else {
+            System.out.println("El template de huella es null, no se puede guardar la huella.");
         }
     }
 
@@ -231,14 +197,10 @@ public class CapturaHuella extends javax.swing.JFrame {
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnGuardar1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblImagenHuella;
     private javax.swing.JPanel panBtns;
-    private javax.swing.JPanel panBtns1;
     private javax.swing.JPanel panHuellas;
     private javax.swing.JTextArea txtArea;
-    private javax.swing.JTextArea txtArea1;
     // End of variables declaration//GEN-END:variables
 }
